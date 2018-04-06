fc109d8d9fc9f6076437e909d84cc538344b7cc2
Jake Steeves <jsteeves@college.harvard.edu>
2018-04-06 13:40:54 -0400

    updated feedback

--- affluentTruffle/contracts/Feedback.sol
+++ affluentTruffle/contracts/Feedback.sol
@@ -4,10 +4,6 @@ contract Feedback {
   mapping (address => string) private userFeedback;
   address public owner;
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
-
   function addFeedback(string feedback) public returns (string) {
     userFeedback[msg.sender] = feedback;
     return userFeedback[msg.sender];
@@ -17,4 +13,5 @@ contract Feedback {
   function viewFeedback() constant returns (string) {
     return userFeedback[msg.sender];
   }
+
 }
