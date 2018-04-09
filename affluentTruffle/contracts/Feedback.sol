65e6fca8fecfd56075ad867bc425a4e2dfbac65c
William Qian <wqian94@gmail.com>
2018-04-08 21:11:50 -0400

    Fixed tests and Feedback contract
    Added tests for retrieving feedback

--- affluentTruffle/contracts/Feedback.sol
+++ affluentTruffle/contracts/Feedback.sol
@@ -1,54 +1,74 @@
 pragma solidity ^0.4.21;
 
 contract Feedback {
-  struct FeedbackQ {
-    mapping (bool => uint) voteCount;
-    uint questionID;
-    uint courseID;
-    bytes32[1024] question;
-  }
-
   address public instructor;
 
+  struct FeedbackQ {
+    uint questionID;
+    uint courseID;
+    bytes question;
+    mapping (bool => uint) voteCount;
+  }
+
+  FeedbackQ[] private daily_questions;
   address[] private enrolled_students;
-  FeedbackQ[] private daily_questions; 
 
-  event Transfer(address indexed _from, address indexed _to, uint256 _value);
-  event Propagate(address indexed _from, bool _value);
+  //event Propagate(address indexed _from, bool _value);
 
-  function validateInstructor() public returns (bool validated) {
-    instructor = msg.sender;
-    emit Propagate(instructor, true);
-    return true;
+  function newEnrollment(address student_addr) public returns (uint student_num) {
+    return enrolled_students.push(student_addr)-1;
+  }
+ 
+  function newQuestion(uint questionID, uint courseID, bytes text) public {
+    uint rownumber = daily_questions.length;
+    daily_questions.length++;
+    FeedbackQ storage question = daily_questions[rownumber];
+    question.questionID = questionID;
+    question.courseID = courseID;
+    question.question = text;
+    question.voteCount[false] = 0;
+    question.voteCount[true] = 0;
   }
 
-  function newEnrollment() public returns (uint student_num) {
-    emit Transfer(msg.sender, instructor, 1);
-    return enrolled_students.push(msg.sender)-1;
-  }
-
-  function newQuestion(uint questionID, uint courseID, bytes32[1024] question) public
-                          returns(uint rownumber) {
-    FeedbackQ memory newDailyQuestion;
-    newDailyQuestion.questionID = questionID;
-    newDailyQuestion.courseID = courseID;
-    newDailyQuestion.question = question;
-    emit Transfer(msg.sender, instructor, 1);
-    return daily_questions.push(newDailyQuestion)-1;
-  }
-
-  // view instructions
-
-  function getInstructor() public view returns (address retinstructor) {
-    return instructor;
-  }
-  
-  function studentsCount() public view returns(uint studentCount) {
-    return enrolled_students.length;
-  }
-
-  function questionsCount() public view returns(uint questionCount) {
+  function questionsCount() public constant returns(uint questionCount) {
     return daily_questions.length;
   }
 
+  function studentsCount() public constant returns(uint studentCount) {
+    return enrolled_students.length;
+  }
+
+  function giveFeedback(bool feedback, uint questionID) public {
+    uint studcount = studentsCount();
+    uint j = 0;
+    bool inclass = false;
+    for (j; j < studcount; j++ ) {
+      if (enrolled_students[j] == msg.sender) {
+        inclass = true;
+        break;
+      }
+    }
+
+    require(inclass);
+
+    uint count = questionsCount();
+    uint i = 0;
+
+    for (i; i < count; i++) {
+      if (daily_questions[i].questionID == questionID) {
+        daily_questions[i].voteCount[feedback]++;
+      }
+    }
+  }
+
+  function viewFeedback(uint questionID, bool responsetype) public
+                            view returns (uint result) {
+    uint count = questionsCount();
+    uint i = 0;
+    for (i; i < count; i++) {
+      if (daily_questions[i].questionID == questionID) {
+        return daily_questions[i].voteCount[responsetype];
+      }
+    }
+  }
 }
--- affluentTruffle/test/feedback.js
+++ affluentTruffle/test/feedback.js
@@ -1,35 +1,116 @@
 var Feedback = artifacts.require("./Feedback.sol");
 
-contract('Feedback', function(accounts) {
-  var instructor = accounts[0];
-  var enrollment = 5;
+contract('Feedback basic test', (accounts) => {
+  const enrollment = 5;
 
-  it("Testing instructor validation...", function() {
-    return Feedback.deployed().then(function(instance) {
-      instance.validateInstructor({from: instructor});
-      return instance.getInstructor();
-    }).then(function(ret) {
-      assert.equal(ret, instructor, "Instructor is confirmed to be the account.");
-    });
+  let instance;  // The instance to use for each test
+  beforeEach(async () => {
+    instance = await Feedback.new();
   });
-  
-  it("Testing student enrollment...", function() {
-    return Feedback.deployed().then(function(instance) {
-      for (var i = 1; i <= enrollment; i ++) {
-        instance.newEnrollment({from: accounts[i]});
+
+  it("should see 1 enrolled student", async () => {
+    await instance.newEnrollment(accounts[1]);
+    assert((await instance.studentsCount()), 1,
+           "Singular student account enrollment failed");
+  });
+
+  it("should see " + enrollment.toString() + " enrolled students", async () => {
+    for (var i = 1; i <= enrollment; i++) {
+      instance.newEnrollment(accounts[i]);
+    }
+    assert.equal((await instance.studentsCount()).toNumber(), enrollment,
+                 "Multiple student account enrollments failed");
+  });
+
+/*
+  it("Testing instructor validation...", async () => {
+    instance.validateInstructor({from: instructor});
+    assert.equal((await instance.getInstructor()), instructor,
+                 "Instructor is confirmed to be the account.");
+  });
+*/
+
+  it("should see 1 question added", async () => {
+    instance.newQuestion(0, 144, "Do you like this class?");
+    assert.equal((await instance.questionsCount.call()).toNumber(), 1,
+                 "Singular question addition failed");
+  });
+
+  it("should see 3 questions added", async () => {
+    instance.newQuestion(0, 144, "Do you like this class?");
+    instance.newQuestion(1, 244, "Do you like this class?");
+    instance.newQuestion(2, 50, "Do you like this class?");
+    assert.equal((await instance.questionsCount.call()).toNumber(), 3,
+                 "Multiple question addition failed");
+  });
+});
+
+contract('Feedback unit tests', (accounts) => {
+  const instructor = accounts[0];
+  const enrollment = 5;
+
+  let instance;  // The instance to use for each test
+  beforeEach(async () => {
+    instance = await Feedback.new();
+    for (var i = 1; i <= enrollment; i++) {
+      instance.newEnrollment(accounts[i]);
+    }
+    instance.newQuestion(0, 144, "Do you like this class?");
+    instance.newQuestion(1, 244, "Do you like this class?");
+    instance.newQuestion(2, 50, "Do you like this class?");
+  });
+
+  // expect should be an array of form [[expect, qnum, responsetype], [...], ..]
+  const expectFeedback = async (instance, expect) => {
+    for (var test of expect) {
+      assert.equal(
+        (await instance.viewFeedback(test[1], test[2])).toNumber(), test[0],
+        `Feedback for question ${test[1]}, responsetype ${test[2]} incorrect`);
+    }
+  }
+
+
+  it("should see empty statistics for all three questions", async () => {
+    const expect = [
+      [0, 0, false],
+      [0, 0, true],
+      [0, 1, false],
+      [0, 1, true],
+      [0, 2, false],
+      [0, 2, true],
+    ];
+    await expectFeedback(instance, expect);
+  });
+
+  it("should see 1 count for each (question, answer) pair", async () => {
+    const expect = [
+      [1, 0, false],
+      [1, 0, true],
+      [1, 1, false],
+      [1, 1, true],
+      [1, 2, false],
+      [1, 2, true],
+    ];
+    for (var test of expect) {
+      instance.giveFeedback(test[2], test[1], {from: accounts[1]});
+    }
+    await expectFeedback(instance, expect);
+  });
+
+  it("should see correct counts for each (question, answer) pair", async () => {
+    const expect = [
+      [2, 0, false],
+      [3, 0, true],
+      [5, 1, false],
+      [4, 1, true],
+      [1, 2, false],
+      [0, 2, true],
+    ];
+    for (var test of expect) {
+      for (var i = 0; i < test[0]; i++) {  // To account for the previous test
+        instance.giveFeedback(test[2], test[1], {from: accounts[i + 1]});
       }
-      return instance.studentsCount();
-    }).then(function(ret) {
-      assert.equal(ret, enrollment, "All student accounts enrolled.");
-    });
-  });
-
-  it("Testing question adding", function() {
-    return Feedback.deployed().then(function(instance) {
-      instance.newQuestion(0, 144, "Do you like this class?", {from: accounts[enrollment+1]});
-      return instance.questionsCount();
-    }).then(function(ret) {
-      assert.equal(ret, 1, "First question added.");
-    });
+    }
+    await expectFeedback(instance, expect);
   });
 });
