d383c0100a8938cad455762aa9508a7eddab4925
Jake <jsteeves@college.harvard.edu>
2018-05-03 14:37:34 -0400

    made tests for course creation, approval/activation/de, enroll/drop

--- test/affluent_test.js
+++ test/affluent_test.js
@@ -60,19 +60,19 @@ contract('Testing affluent application functions', (accounts) => {
 
       let cls_addr = await instance.getClass.call(index);
       assert.equal(cls_addr, addr, 
-        "Class contract address does not match earlier stored record");
+        `${info.label}: class contract address does not match earlier stored record`);
 
       let cls_instance = await contracts.Class.at(cls_addr);
       let cls_instructor = await cls_instance.getInstructor.call();
 
       assert.equal(cls_instructor, info.instructor,
-        "Failed to set class instructor to the correct address of the sender");
+        `${info.label}: failed to set class instructor to the address of the sender`);
       assert.equal(await cls_instance.getLabel.call(), info.label,
-        "Failed to set the correct course label");
+        `${info.label}: failed to set the correct course label`);
       assert.equal(await cls_instance.getTerm.call(), info.term,
-        "Failed to set the correct course term");
+        `${info.label}: failed to set the correct course term`);
       assert.equal(await cls_instance.getTitle.call(), info.title,
-        "Failed to set the correct course title");
+        `${info.label}: failed to set the correct course title`);
     });
   });
 
@@ -81,17 +81,79 @@ contract('Testing affluent application functions', (accounts) => {
     course_addrs.forEach(async (val, index) => {
       let info = val.info;
       let addr = val.addr;
-
       let cls_instance = await contracts.Class.at(addr);
+
       assert.equal(await cls_instance.isActive.call(), false, 
-        "Class is supposed to be inactive on initialization");
+        `${info.label}: class contract should be inactive on initialization`);
 
       await cls_instance.activate({from: info.instructor});       
 
       assert.equal(await cls_instance.isActive.call(), true, 
-        "Class contract failed to be activated by the instructor");
+        `${info.label}: class contract failed to be activated by the instructor`);
+    });
+  });
+
+  it("Test deactivation of the courses, then reactivate for later tests", async () => {
+    course_addrs.forEach(async (val, index) => {
+      let info = val.info;
+      let addr = val.addr;
+      let cls_instance = await contracts.Class.at(addr);
+
+      // wait for activation
+      let active;
+      while (!active)
+        active = await cls_instance.isActive.call();
+
+      assert.equal(await cls_instance.isActive.call(), true, 
+        `${info.label} is inactive`);
+  
+      await cls_instance.deactivate({from: info.instructor});
+      assert.equal(await cls_instance.isActive.call(), false, 
+        `Failed to deactivate ${info.label}`);
+
+      await cls_instance.activate({from: info.instructor});       
+      assert.equal(await cls_instance.isActive.call(), true, 
+        `Failed to reactivate ${info.label}`);
 
     });
   });
 
+  it("Enroll 5 students in each course, then individually verify their enrollment", async () => {
+    course_addrs.forEach(async (val, index) => {
+      let info = val.info;
+      let addr = val.addr;
+      let cls_instance = await contracts.Class.at(addr);
+
+      let start = 3 + (5 * index);
+      let end = start + 5
+      for (let i = start; i < end; i++) {
+        await cls_instance.enroll(accounts[i], {from: info.instructor});       
+        assert.equal(await cls_instance.isEnrolled.call(accounts[i]), true,
+          `Failed to enroll ${accounts[i]} in ${info.label}`);
+      }
+    });
+  });
+
+  it("Test that the instructor can drop a student from a course", async () => {
+    course_addrs.forEach(async (val, index) => {
+      let info = val.info;
+      let addr = val.addr;
+      let cls_instance = await contracts.Class.at(addr);
+
+      let start = 3 + (5 * index);
+      let drop = start + 4
+
+      let enrolled;
+      while (!enrolled)
+        enrolled = await cls_instance.isEnrolled.call(accounts[drop]);
+  
+      assert.equal(await cls_instance.isEnrolled.call(accounts[drop]), true, 
+        `${accounts[drop]} is not enrolled in ${info.label}`);
+
+      await cls_instance.drop(accounts[drop], {from: info.instructor});
+      
+      assert.equal(await cls_instance.isEnrolled.call(accounts[drop]), false,
+        `Failed to drop ${accounts[drop]} from ${info.label}`);
+    });
+  });
 });
